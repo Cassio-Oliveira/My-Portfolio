4c03bdaf1a6401a340f2fa950844856122626649
Cassio-Oliveira <66282366+Cassio-Oliveira@users.noreply.github.com>
2020-07-02 14:29:57 -0300

    Code

---
+++ code/script_jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){          
+	$('#enviar').click(function(){
+        $("#id01").remove();    
+        $(".proposta").remove();
+        $('#mensagem').text("Registro feito com sucesso.")
+        
+	});
+});
